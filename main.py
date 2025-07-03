3ba3386dbbd60349ef3efcb2661ef13668985f3b
André Borge <andreborge1008@gmail.com>
2025-07-03 21:56:46 +0200

    Small bug fixes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord import app_commands
 import requests
 import os
 import asyncio
 from dotenv import load_dotenv
+from discord.errors import NotFound
 
 load_dotenv()
 COC_API_KEY = os.getenv("COC_API_KEY")
@@ -13,10 +15,12 @@ headers = {"Authorization": f"Bearer {COC_API_KEY}"}
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
 GUILD_ID = 1371206811901890682
 CHANNEL_ID = 1387429772023562260
 CLAN_INFO_MESSAGE_ID = 1387433042548756672
+STATUS_CHANNEL_ID = 1387771579328630856
 
 CLANS = {
     "Royal Legion": {
@@ -40,32 +44,24 @@ async def fetch_clan_data_async(clan_tag):
                 "war_Wins": data.get("warWins"),
                 "war_Losses": data.get("warLosses"),
                 "war_Ties": data.get("warTies"),
+                # there is more data you can fetch, check out the Clash of Clans API documentation for more details.
             }
         else:
             print(f"Error fetching clan {clan_tag}: {response.status_code}")
             return None
     return await asyncio.to_thread(fetch)
-
+# all events
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=intents)
         self.synced = False
         
 
-    async def setup_hook(self):
-        if not self.synced:
-            await self.tree.sync(guild=discord.Object(id=GUILD_ID))
-            self.synced = True
-            print("Slash commands synced.")
-        self.bg_task = asyncio.create_task(self.update_clan_data_loop())
+    
 
-    async def on_ready(self):
-        print(f"Logged in as {self.user}")
-        
-        channel = self.get_channel(CHANNEL_ID)
-        
-
+    
     async def update_clan_data_loop(self):
+    
         await self.wait_until_ready()
         while not self.is_closed():
             print("Fetching clan data...")
@@ -91,7 +87,7 @@ class MyBot(commands.Bot):
                     description=clans_desc,
                     color=0x96f348
                 )
-                embed.add_field(
+                embed.add_field( # basic embed 
                     name="__What We Offer__",
                     value=(
                         "Casual\n"
@@ -107,7 +103,7 @@ class MyBot(commands.Bot):
                 )
                 embed.set_thumbnail(url=self.user.avatar.url if self.user.avatar else None)
                 embed.set_image(
-                    url="https://cdn.discordapp.com/attachments/1387441529181962260/1387442953215279329/placeholder.png"
+                    url="https://cdn.discordapp.com/attachments/1387441529181962260/1387442953215279329/placeholder.png" #Placeholder image, replace with actual image if needed. Can also be removed.
                 )
                 #await message.edit(embed=embed)
                 print("Embed updated with latest clan data.")
@@ -116,6 +112,46 @@ class MyBot(commands.Bot):
                 print(f"Failed to update embed: {e}")
             await asyncio.sleep(6 * 3600)  # Every 6 hours
 
+    # the visual status report
+    def generate_status_report(self):
+        print("status report sent")
+        return "**Placeholder**"
+    # status report func
+    @tasks.loop(minutes=60)
+    async def status_report(self):
+        await self.wait_until_ready()
+        channel = self.get_channel(STATUS_CHANNEL_ID)
+        if channel:
+            report = self.generate_status_report()
+            await channel.send(report)
+        else:
+            print("Report Channel Not found.")
+
+    # quick /clear command to clear a channel for all of its messages(useful in development). 
+    @app_commands.command(name="clear", description="simple command that removes all the previous messages sent in a channel")
+    async def clear(interaction: discord.Interaction, amount: int = 10):
+        await interaction.response.defer()
+        await interaction.channel.purge(limit=amount + 1)
+
+        try:
+            await interaction.followup.send(f"Cleared {amount} messages", ephemeral=True)
+        except NotFound:
+            print("Could not send follow-up, interaction message was deleted(/clear command)")
+    
+    async def setup_hook(self):
+        self.tree.add_command(self.clear)
+        
+        self.bg_task = asyncio.create_task(self.update_clan_data_loop())
+        self.status_report.start()
+        await self.tree.sync(guild=discord.Object(id=GUILD_ID))
+        
+
+    # everything that happends on startup
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+        print("report ")
+        channel = self.get_channel(CHANNEL_ID)
+        await self.tree.sync()
 
 # Dropdown Menu For clans
 class ClanDropdown(discord.ui.Select):
@@ -123,7 +159,7 @@ class ClanDropdown(discord.ui.Select):
         options = [
             discord.SelectOption(label="Royal Legion", description="View Royal Legion clan details"),
             discord.SelectOption(label="Royal Dukedome", description="View Royal Dukedome clan details"),
-            # here you can add more clans based on your needs
+            # here you can add more clans based on your needs, clans mentioned are only examples. These clans will show up in the embed dropdown menu.
         ]
         super().__init__(placeholder="Choose a clan to view.....", min_values=1, max_values=1, options=options)
     async def callback(self, interaction: discord.Interaction):
@@ -149,6 +185,27 @@ class ClanDropdownView(discord.ui.View):
         self.add_item(ClanDropdown())
 
 
+    
 
+# quick /clear command to clear a channel for all of its messages(useful in development)
+@bot.command()
+async def clear (ctx, amount = 5):
+    await ctx.channel.purge (limit = amount)
+    await ctx.send (f"Cleared {amount} messages!")
+
+
+
+async def on_ready():
+    guild = discord.Object(id=GUILD_ID)
+    await bot.tree.sync()
 bot = MyBot()
+
+
+    
+
 bot.run(DISCORD_TOKEN)
+
+
+
+
+
